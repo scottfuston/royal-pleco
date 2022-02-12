962986dd9dea4a438f7f3d446ea444638e91e46e
scottfuston <scott.fuston@grandpad.net>
2022-02-12 11:35:15 -0500

    fixes things

--- models/jira_model.js
+++ models/jira_model.js
@@ -7,50 +7,38 @@ module.exports = {
 };
 
 // get issue
-async function getIssue(issueNum) {
+function getIssue(issueNum) {
   // call jira api and get current issue(card)
   let url = `${process.env.JIRA_URL}/rest/api/2/issue/${issueNum}`;
-  try {
-    const res = await axios({
-      method: "get",
-      url: url,
-      auth: {
-        username: process.env.USERNAME,
-        password: process.env.PASSWORD,
-      },
-    });
 
-    return res;
-  } catch (err) {
-    return err;
-  }
+  return axios({
+    method: "get",
+    url: url,
+    auth: {
+      username: process.env.USERNAME,
+      password: process.env.PASSWORD,
+    },
+  });
 }
 
 // set testRail description
-async function setTestRailDescription(descStr, run_id) {
-  console.log("descStr: ", descStr, "run_id: ", run_id);
+function setTestRailDescription(descStr, run_id) {
+  // console.log("descStr: ", descStr, "run_id: ", run_id);
 
   let url = `${process.env.TEST_RAIL_URL}/index.php?/api/v2/update_run/${run_id}`;
-  try {
-    const res = await axios({
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin" : "*"
-      },
-      url: url,
-      auth: {
-        username: process.env.EMAIL,
-        password: process.env.PASSWORD,
-      },
-      data: {
-        description: descStr,
-      },
-    });
-
-    console.log("setTestRailDescription finished: ");
-    return res;
-  } catch (err) {
-    return err;
-  }
+  return axios({
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+    url: url,
+    auth: {
+      username: process.env.EMAIL,
+      password: process.env.PASSWORD,
+    },
+    data: {
+      description: descStr,
+    },
+  });
 }
--- routers/jira_router.js
+++ routers/jira_router.js
@@ -11,96 +11,67 @@ router.get("/", async (req, res) => {
   const jira_issue = req.query.jira_issue;
   const run_id = req.query.run_id.trim();
 
-  try {
-    const issueStr = await jira.getIssue(jira_issue);
+  jira
+    .getIssue(jira_issue)
+    .then((issRes) => {
+      // console.log("issRes: ", issRes);
 
-    if (!issueStr.data.fields) {
-      console.log("issueStr: ", issueStr.data.errorMessages[0]);
-      return res
-        .status(404)
-        .send({ Error: issueStr.response.data.errorMessages[0] });
-    }
+      if (!issRes.data.fields) {
+        console.log("issRes: ", issRes.data.errorMessages[0]);
+        return res
+          .status(404)
+          .send({ Error: issRes.response.data.errorMessages[0] });
+      }
 
-    // filter out 'Bugs Found' types
-    const issueLinks = issueStr.data.fields.issuelinks.filter((iss) => {
-      return iss.type.name !== "Bugs Found";
+      // filter out 'Bugs Found' types
+      const issueLinks = issRes.data.fields.issuelinks.filter((iss) => {
+        return iss.type.name !== "Bugs Found";
+      });
+
+      // init header str
+      let resArray = issueLinks.length > 0 ? ["|||Issue|Tester"] : [];
+
+      issueLinks.forEach((ele) => {
+        let type = ele.inwardIssue
+          ? "inwardIssue"
+          : ele.outwardIssue
+          ? "outwardIssue"
+          : "";
+        const key = ele[type].key;
+        const description = ele[type].fields.summary;
+
+        resArray.push(`||${key}: ${description}|`);
+      });
+
+      // format the description string
+      let descStr = "";
+      resArray.forEach((r) => {
+        descStr += r + "\n";
+      });
+
+      jira
+        .setTestRailDescription(descStr, run_id)
+        .then((descRes) => {
+          console.log("setTestRailDescription Success!: ", descRes.status, "\n\n");
+          if (descRes.status === 200) {
+            res.status(200).send('Success!');
+          }
+        })
+        .catch((descErr) => {
+          const status= descErr.response.status
+          console.log("descErr: ", descErr.response);
+          res.status(status).json({ error: descErr.response.data.error });
+        });
+    })
+
+    .catch((issueErr) => {
+      const error = issueErr.response.data.errorMessages[0];
+      const status= issueErr.response.status
+
+      console.log("issueErr: ", issueErr.response);
+      return res.status(status).send(error);
     });
 
-    // init header str
-    let resArray = issueLinks.length > 0 ? ["|||Issue|Tester"] : [];
-
-    issueLinks.forEach((ele) => {
-      let type = ele.inwardIssue
-        ? "inwardIssue"
-        : ele.outwardIssue
-        ? "outwardIssue"
-        : "";
-      const key = ele[type].key;
-      const description = ele[type].fields.summary;
-
-      resArray.push(`||${key}: ${description}|`);
-    });
-
-    // format the description string
-    let descStr = "";
-    resArray.forEach((r) => {
-      descStr += r + "\n";
-    });
-
-    const testRailRes = await jira.setTestRailDescription(descStr, run_id);
-    console.log("testRailRes: ", testRailRes.response.data.error);
-  } catch (err) {
-    console.log("issueStr err: ", err);
-    return res.status(500).send(err);
-  }
-
-  // jira
-  //   .getIssue(jira_issue)
-  //   .then(async (issue) => {
-  // if (!issue.data) return { Error: issue.response.statusText };
-  // const response = issue.data;
-  // // filter out 'Bugs Found' types
-  // const issueLinks = response.fields.issuelinks.filter((iss) => {
-  //   return iss.type.name !== "Bugs Found";
-  // });
-  // // init header str
-  // let resArray = issueLinks.length > 0 ? ["|||Issue|Tester"] : [];
-  // issueLinks.forEach((ele) => {
-  //   let type = ele.inwardIssue
-  //     ? "inwardIssue"
-  //     : ele.outwardIssue
-  //     ? "outwardIssue"
-  //     : "";
-  //   const key = ele[type].key;
-  //   const description = ele[type].fields.summary;
-  //   resArray.push(`||${key}: ${description}|`);
-  // });
-  // // send data to TestRail api
-  // let descStr = "";
-  // resArray.forEach((r) => {
-  //   descStr += r + "\n";
-  // });
-  // return descStr;
-  // })
-  // .then((descStr) => {
-  //   if (descStr.Error) {
-  //     console.log("descStr: ", descStr.Error);
-  //     return res.status(404).send({ Error: descStr.Error });
-  //   }
-  //   jira
-  //     .setTestRailDescription(descStr, run_id)
-  //     .then((testRailRes) => {
-  //       console.log("testRail res:  ", testRailRes.status);
-  //       return res.status(testRailRes.status).send({ testRailRes });
-  //     })
-  //     .catch((err) => {
-  //       return err;
-  //     });
-  // })
-
-  // .catch((err) => {
-  //   return res.status(500).send({ Error: err });
-  // });
 });
 
 module.exports = router;
